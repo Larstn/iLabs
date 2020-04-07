4bd9f47e655880fa2a3f0b64a6d94d8c11cd6d1c
larstn <larstn@stanford.edu>
2020-04-06 17:31:00 -0700

    Included docstring for WriteStateFile

--- WriteStateFile.py
+++ WriteStateFile.py
@@ -1,4 +1,13 @@
 def writeStateFile(ExperimentName, controls, booleans, indicators, imagenames, noise = [], NoiseProfiles = []):
+    """Input: ExperimentName, controls, booleans, indicators, imagenames, [noise, NoiseProfiles]
+    ExperimentName: String, Name of the Experiment
+    controls: ListType, List of how the controls are set in this state 
+    boolenas: ListType, List of how the booleans are set in this state
+    indicators: ListType, List of indicator values at this state
+    imagenames: ListType, List of all images associated with this state
+    noise: ListType, if you provide noise type, provide one value for each indicator. Empty element, if poission or no noise, std if gaussian noise
+    NoiseProfiles = ListType, provide noise with the profiles, List of strings, use "g" for Gaussian, "p" for Poission and "n" for no noise type or other noise
+  """
   FileName = "{}.txt".format(ExperimentName)
   iLabsFile = open(FileName, "a")
 
@@ -9,12 +18,10 @@ def writeStateFile(ExperimentName, controls, booleans, indicators, imagenames, n
   if noise != []: 
     indicatorList = []
     for n, NoiseProfile in enumerate(NoiseProfiles): 
-      print("n: {}, NoiseProfile: {}".format(n, NoiseProfile))
       if NoiseProfile == 'g':
         indicatorList.append(str(indicators[n])+":"+str(noise[n]))
       else: 
         indicatorList.append(str(indicators[n]))
-    print("Indicator List: {}".format(indicatorList))
     indicatorString = ','.join(indicatorList)
   else: 
     indicatorString = ','.join([str(indicator) for indicator in indicators])
