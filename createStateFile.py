5eb95b91ab6b8935b3b64045c7c6a6bd21a28469
larstn <larstn@stanford.edu>
2020-04-06 17:24:45 -0700

    Included docstring for createStateFile

--- createStateFile.py
+++ createStateFile.py
@@ -1,4 +1,11 @@
 def createStateFile(ExperimentName, Ncontrols, Nbooleans, Nindicators, NoiseProfiles = []):
+"""creates the file to store the state data, Input: ExperimentName, NControls, NBooleans, Nindicators, [NoiseProfiles]"
+	Experiment Name: Name of your experiment
+	NControls: Number of numeric controls in the experiment
+	NBooleans: Number of boolean controls in the experiment 
+	Nindicators: Number of Indicators (data displays) in the experiment
+	NoiseProfile [optional]: Noise Profile of the Indicators. String Type. If gaussian, use "g", if Poisson use "p", if no noise use "n"
+"""
   FileName = "{}.txt".format(ExperimentName)
   iLabsFile = open(FileName, "w")
 
