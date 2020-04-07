49c1a3792115ce7905081eb3e0e9bdc5660d15ad
larstn <larstn@stanford.edu>
2020-04-06 17:20:24 -0700

    Changes to Camera Function

--- TakePicturePICam.py
+++ TakePicturePICam.py
@@ -1,8 +1,11 @@
 from picamera import PiCamera
 from time import sleep
 
+camera = PiCamera()
 
  def takePicture(ExperimentName, camera, N):
+ 	"""Takes Picture, input arguments:
+ 	ExperimentName: Name of your experiment, camera: camera object, N: individual identifier number for the image"""
  	ImageFilename = ExperimentName + 'Image{}.jpg'.format(N)
     camera.capture(ImageFilename)
     sleep(1)
